9100046d545c72036d50b373c21a033e055496c3
Ronan F. <ronan.fesselier@etik.com>
2025-12-02 07:25:46 +0100

    refactor(cmake): reorganize build configuration and improve package portability
    - Move installation prefix setup to nfxDateTimeBuildConfig.cmake
    - Move license metadata to build config module
    - Remove redundant GNUInstallDirs inclusion in install script
    - Add PATH_VARS to configure_package_config_file for relocatable packages
    - Decentralize dependency management to test/benchmark modules
    - Remove nfxDateTimeDependencies.cmake in favor of modular approach
    - Standardize dependency version variable naming

--- cmake/nfxDateTimeInstall.cmake
+++ cmake/nfxDateTimeInstall.cmake
@@ -12,19 +12,6 @@ if(NOT NFX_DATETIME_INSTALL_PROJECT)
 endif()
 
 #----------------------------------------------
-# Installation paths configuration
-#----------------------------------------------
-
-include(GNUInstallDirs)
-
-message(STATUS "System installation paths:")
-message(STATUS "  Headers      : ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
-message(STATUS "  Library      : ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
-message(STATUS "  Binaries     : ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
-message(STATUS "  CMake configs: ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/cmake/nfx-datetime")
-message(STATUS "  Documentation: ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_DOCDIR}")
-
-#----------------------------------------------
 # Install headers
 #----------------------------------------------
 
@@ -112,6 +99,7 @@ configure_package_config_file(
 	"${CMAKE_CURRENT_SOURCE_DIR}/cmake/nfx-datetime-config.cmake.in"
 	"${CMAKE_CURRENT_BINARY_DIR}/nfx-datetime-config.cmake"
 	INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/nfx-datetime
+	PATH_VARS CMAKE_INSTALL_INCLUDEDIR CMAKE_INSTALL_LIBDIR
 )
 
 install(
