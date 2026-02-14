df5805556bbb53e5be5a1962942b0c7fabe62c97
Ronan F․ <ronan.fesselier@etik.com>
2026-02-14 12:53:06 +0100

    chore(deps): bump nfx-stringbuilder from 0.6.0 to 0.6.2

--- cmake/nfxDateTimeDependencies.cmake
+++ cmake/nfxDateTimeDependencies.cmake
@@ -18,7 +18,7 @@ set(CMAKE_FIND_QUIETLY      ON     )
 # Dependency versions
 #----------------------------------------------
 
-set(NFX_DATETIME_DEPS_NFX_STRINGBUILDER_VERSION "0.6.0")
+set(NFX_DATETIME_DEPS_NFX_STRINGBUILDER_VERSION "0.6.2")
 
 #----------------------------------------------
 # FetchContent dependencies
